298eb83dbd39a843e66640daac2ca65cfbacfc60
fnkbsk <igoico10@gmail.com>
2023-11-24 09:36:11 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,15 @@ import './App.css';
 import { TaskList, TaskForm, TaskItem } from './components/index.js';
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const [searchString, setSearchString] = useState('');
 
-  const addTask = (task) => {
-    setTasks([...tasks, task])
-  }
-
-  const deleteTask = (taskId) => {
-    setTasks(tasks.filter(task => task.id != taskId))
-  }
-
-  const handleChangeFilter = (e) => {
-    setSearchString(e.target.value);
-  }
 
   return (
     <>
       <div>
         
       </div>
-        <TaskForm addTask={addTask}/>
-        <TaskList tasks={tasks} deleteTask={deleteTask}/>
+        <TaskForm />
+        <TaskList />
     </>
     
   )
--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,51 +1,28 @@
 import React from "react";
+import { FaPlus } from "react-icons/fa";
 import "./TaskForm.css";
 
-
 const TaskForm = () => {
-  const [isOpenModal, setIsOpenModal] = useState(false);
-
-  const handleCloseModal = () => {
-    setIsOpenModal(false);
-  };
-
-  const handleOpenModal = () => {
-    setIsOpenModal(true);
-  };
-
   return (
     <div>
-      <button onClick={handleOpenModal}>Nueva tarea </button>
-      {isOpenModal && (
-        <div className="modal-background">
-          <div className="modal">
-            <h2>Agregar nueva tarea</h2>
-            <form>
-              <div className="input-container">
-                <label htmlFor="">Ingresa el titulo de tu tarea:</label>
-                <input
-                  type="text"
-                  id="title"
-                  name="title"
-                  placeholder="Titulo ejemplo"
-                />
-              </div>
-              <div className="input-container">
-                <label htmlFor="">Ingresa una descripción:</label>
-                <textarea
-                  name="description"
-                  id="description"
-                  placeholder="Alguna aclaracion/descripcion"
-                ></textarea>
-              </div>
-              <div className="btn-container">
-                <button onClick={handleCloseModal}>Cerrar</button>
-                <button type="submit">Agregar</button>
-              </div>
-            </form>
-          </div>
+      <button>
+        Nueva tarea <FaPlus />
+      </button>
+      <div className="modal-background">
+        <div className="modal">
+          <h2>Agregar una nueva tarea</h2>
+          <form>
+            <div className="input-container">
+              <label htmlFor="title">Ingresa el titulo de tu tarea</label>
+              <input type="text" id="title" name="title" placeholder="Titulo"/>
+            </div>
+            <div className="input-container">
+              <label htmlFor="description">Ingrese una descripcion</label>
+              <input type="text" id="description" name="description" placeholder="Descripcion"/>
+            </div>
+          </form>
         </div>
-      )}
+      </div>
     </div>
   );
 };
