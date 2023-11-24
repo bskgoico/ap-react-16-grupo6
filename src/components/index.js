c54566b40cf285951d0379f843a4490b6ece0ade
fnkbsk <igoico10@gmail.com>
2023-11-24 09:22:14 -0300

    commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,5 @@
-import Archivo from "./Archivo/Archivo";
-import Header from "./Header/Header";
-import Footer from "./Footer/Footer";
-import Product from "./Product/Product";
-
 import TaskForm from "./TaskForm/TaskForm";
 import TaskItem from "./TaskItem/TaskItem";
 import TaskList from "./TaskList/TaskList";
 
-
-export { Archivo, Header, Footer, Product }
 export { TaskList, TaskForm, TaskItem }
